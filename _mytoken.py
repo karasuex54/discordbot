63941f53694ec69a8817acc536a067249f44d85b
karasuex54 <karasuyama1997@gmail.com>
2020-11-08 18:40:18 +0900

    init mytoken.py update

--- _mytoken.py
+++ _mytoken.py
@@ -1,11 +1,18 @@
-TOKEN_KEY = "your discord token"
-DEVELOPER_ID = "your id"
-DATABASE_URL = 'database_url'
+TOKEN_KEY = ''
+DEVELOPER_ID = ''
+DATABASE_URL = ''
 
-def token_key ():
+AMONGUS_GUILD_ID = ''
+AMONGUS_CHANNEL_ID = ''
+
+def token_key():
     return TOKEN_KEY
-def developer_id ():
-    return developer_id
+def developer_id():
+    return DEVELOPER_ID
 def database_url():
     return 'sqlite:///test.db'
     #return DATABASE_URL
+def amongus_guild_id():
+    return AMONGUS_GUILD_ID
+def amongus_channel_id():
+    return AMONGUS_CHANNEL_ID
